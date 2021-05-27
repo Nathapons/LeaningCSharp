b8ae56645a342097de39687fab8b7a646c6c1905
Nathapon.S <nuthaponm79@gmail.com>
2021-05-27 23:00:47 +0700

    first commit

---
+++ Loop/Loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i = i + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
