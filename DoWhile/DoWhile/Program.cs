3fcf713c8f150c00d1830baf8f610d77b5081408
Nathapon.S <nuthaponm79@gmail.com>
2021-05-27 23:27:44 +0700

    create DoWhile Learning

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do while => action before check condition
+            // while => check condition before action
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Hello" + count);
+                count++;
+            } while (count < 10);
+            Console.ReadLine();
+        }
+    }
+}
