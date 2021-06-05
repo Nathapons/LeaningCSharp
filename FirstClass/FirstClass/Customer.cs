18855a7b6f4f0687d60faea3a7370a0ccc8368a6
Nathapon.S <nuthaponm79@gmail.com>
2021-06-05 22:18:54 +0700

    Learn C# Class: How to assign class, GET, and SET

---
+++ FirstClass/FirstClass/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    class Customer
+    {
+        string name = "";
+        string lastName = "";
+        string address = "";
+        string email = "";
+
+        // public string ชื่อ Excapsolation { get => name; set => name = value; }
+        public string Name { get => name; set => name = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+    }
+}
